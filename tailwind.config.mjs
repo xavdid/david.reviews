21e7ef2b64e825b690c241487b67aa3dc014909a
David Brownman <beamneocube@gmail.com>
2023-10-28 23:30:02 -0700

    add tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+
+const { blue, red, emerald } = require("tailwindcss/colors");
+
+const insetShadow = (color) => `inset 0em -0.3em ${color["600"]}`;
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        // have to pre-declare these since there's some weird ordering when conditionally applying the one-offs
+        insetGame: insetShadow(blue),
+        insetMovie: insetShadow(red),
+        insetBook: insetShadow(emerald),
+      },
+      colors: {
+        // .orange {
+        // 	color: #ff9700;
+        // }
+
+        // .blue {
+        // 	color: #0f89ff;
+        // }
+
+        // .purple {
+        // 	color: #930eff;
+        // }
+
+        // .darkgreen {
+        // 	color: #68c800;
+        // }
+        game: {
+          500: "#0f89ff",
+        },
+        movie: {
+          500: "#ff9700",
+        },
+        book: {
+          500: "#930eff",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
