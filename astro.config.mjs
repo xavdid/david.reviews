5bdb387b0efedcb367586571b93611c8002959a2
David Brownman <beamneocube@gmail.com>
2023-12-23 19:46:51 -0800

    add sitemap
    ( completes task-519 )

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://david.reviews",
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), sitemap()],
   image: {
     remotePatterns: [
       // these are intentionally commented out
