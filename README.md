ab67967f3f0fa7d4534fb51f2714cfef2f3b4e9d
David Brownman <beamneocube@gmail.com>
2024-07-11 00:11:09 -0700

    add series completion info to series page

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 ➡️ https://david.reviews ⬅️
 
 This is an [Astro](https://astro.build/) + [Tailwind](https://tailwindcss.com/) site which collects all of my media micro-reviews. Data is stored in [Airtable](https://airtable.com/) an loaded using a custom ORM. It ships remarkably little JS to the frontend.
+
+## ORM
+
+I wrote a [custom typed ORM](https://xavd.id/blog/post/static-review-site-with-airtable/) for loading Airtable data. To add new fields:
+
+1. Go to [the docs page](https://airtable.com/developers/web/api/introduction) and select your base
+2. Find the `fld...` ids you're adding
+3. Add them (and a name) to the `SCHEMA.fields` object in the `airtable/data/<BASE>.ts` file
+4. If any added fields aren't `string`s, add them (by `field.NAME`) to `NonStringFields`
+5. Add the relevant output fields to the exported type. This represents the object returned by `materialize` (which turns a row into a usable object)
+6. Ensure those new types get materialized
--- src/airtable/data/series.ts
+++ src/airtable/data/series.ts
@@ -8,29 +8,59 @@ const SCHEMA = {
   tableName: "Series",
   fields: {
     name: "fldYMhfaWJn4oUyB5",
+    numBooksInSeries: "fldKT1QGhdmSHQUdV",
+    rating: "fldbvHodkWRdCOEp2",
+    notes: "fldpkcCrdWSYhKFd9",
+    percentComplete: "flduJfhZdW8nOV8lM",
   },
 } as const satisfies AirtableBase;
 const fields = SCHEMA.fields;
 
 type FieldIds = (typeof fields)[keyof typeof fields];
-type StringFields = {
-  [fieldId in FieldIds]: string;
+type NonStringFields = {
+  [fields.rating]?: number;
+  [fields.numBooksInSeries]?: number; // number of books in series, if finished
+  [fields.notes]?: string;
+  [fields.percentComplete]: number; // float perecentage; -1 is unfinished series
 };
-type SeriesRecord = StringFields & RecordBase;
+type StringFields = {
+  [fieldId in Exclude<FieldIds, keyof NonStringFields>]: string;
+};
+type SeriesRecord = StringFields & NonStringFields & RecordBase;
 
 export type Series = {
   name: string;
   slug: string;
   permalink: Permalink;
+  numBooksInSeries?: number;
+  percentComplete?: number;
+  remarks?: {
+    notes: string;
+    rating: number;
+  };
 };
 
 const materialize = (seriesRow: SeriesRecord): Series => {
   const slug = slugify(seriesRow[fields.name]);
-  return {
+  const o: Series = {
     name: seriesRow[fields.name],
+    numBooksInSeries: seriesRow[fields.numBooksInSeries],
+    percentComplete: seriesRow[fields.percentComplete],
     slug,
     permalink: `/books/series/${slug}/`,
   };
+
+  const rating = seriesRow[fields.rating];
+  const notes = seriesRow[fields.notes];
+
+  if (rating != null && notes) {
+    o.remarks = {
+      rating,
+      notes,
+    };
+  }
+
+  return o;
 };
 
 export const loadSeries = async (): Promise<Record<string, Series>> =>
