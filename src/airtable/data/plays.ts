836702ec37d162aa181c04f8433bad35550166b2
David Brownman <oss@xavd.id>
2025-02-08 10:42:11 -0800

    add impressions post

--- src/airtable/data/plays.ts
+++ src/airtable/data/plays.ts
@@ -1,3 +1,4 @@
+import { isProdBuild } from "../../utils/data";
 import type { AirtableBase, RecordBase } from "../types";
 import { loadListedObjects } from "./common";
 import { loadGames, type Game } from "./games";
@@ -71,3 +72,48 @@ export const loadPlays = async (): Promise<Play[]> =>
       keyGrabber: (watchRow) => watchRow[fields.game],
     },
   ]);
+
+const gameToPlays = new Map<string, Play[]>();
+
+/**
+ * get data suitable for adding a playbox to an MDX document. Should only compute data once; all other loads should be fast.
+ * @param slug the slug of the game to load. May change, but the site will be loud about it
+ * @param index All plays are sorted chronologically, earliest to latest. Unless I'm backfilling something, this should be a stable way to point to a play
+ */
+export const getPlayForGame = async (
+  slug: string,
+  index = 0,
+): Promise<Play | null> => {
+  if (gameToPlays.size === 0) {
+    console.log("filling game -> plays map!");
+    const plays = await loadPlays();
+
+    plays.forEach((play) => {
+      gameToPlays.get(play.game.slug)?.push(play) ??
+        gameToPlays.set(play.game.slug, [play]);
+    });
+
+    for (const id of gameToPlays.keys()) {
+      gameToPlays
+        .get(id)
+        ?.sort(
+          (a, b) => Date.parse(b.dateFinished) - Date.parse(a.dateFinished),
+        );
+    }
+  }
+
+  const plays = gameToPlays.get(slug);
+  if (!plays) {
+    const message = `Tried to load review for igdbId: ${slug}, but it wasn't found`;
+    if (isProdBuild) {
+      throw new Error(message);
+    } else {
+      console.warn(
+        `Tried to load review for igdbId: ${slug}, but it wasn't found`,
+      );
+      return null;
+    }
+  }
+
+  return plays[index];
+};
